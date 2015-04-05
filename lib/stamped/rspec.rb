615a7672253f7a83811718544fdbb03bc09b8278
Allen Madsen <blatyo@gmail.com>
2015-04-05 09:54:07 -0400

    Don't require rspec. It should already be available.

--- lib/stamped/rspec.rb
+++ lib/stamped/rspec.rb
@@ -1,5 +1,3 @@
-require 'rspec'
-
 RSpec::Matchers.define :be_timestamp do
   match do |actual|
     @expected = /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z/
--- lib/stamped/version.rb
+++ lib/stamped/version.rb
@@ -1,4 +1,4 @@
 # Stamped version
 module Stamped
-  VERSION = '0.2.0'
+  VERSION = '0.2.1'
 end
